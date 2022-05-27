5608bca0de2b5b2814a77e109a2aec2699dfd6da
coundia <papacoundia@gmail.com>
2022-05-27 20:36:49 +0000

    edit RelativeTimePipe

--- src/app/pipes/relative-time.pipe.ts
+++ src/app/pipes/relative-time.pipe.ts
@@ -1,12 +1,23 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'relativeTime'
 })
 export class RelativeTimePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(seconds: number): string {
+    if (seconds < 15) {
+      return 'a few seconds ago';
+    } else if (seconds < 60) {
+      return 'less than a minute ago';
+    } else if (seconds < 120) {
+      return 'a minute ago';
+    } else if (seconds < 240) {
+      return 'a few minutes ago';
+    } else if (seconds < 3600) {
+      return `${Math.floor(seconds / 60)} minutes ago`;
+    } else if (seconds < 7200) {
+      return 'an hour ago';
+    } else
+      return 'more than an hour ago';
   }
-
 }
